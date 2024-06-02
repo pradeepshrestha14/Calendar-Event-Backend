04d4c1a69a490c0e887e371e3a004af8a5aaf929
Pradeep Shrestha <pse@logpoint.com>
2024-06-02 15:52:52 +0545

    main: code refactor

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-// // Schedule email notifications for all events at server startup
-// const Event = require("./models/event");
-// Event.find().then((events) => {
-//   events.forEach((event) => {
-//     scheduleEmailNotification(event);
-//   });
-// });
+// Schedule email notifications for all events at server startup
+const Event = require("./models/event");
+Event.find().then((events) => {
+  events.forEach((event) => {
+    scheduleEmailNotification(event);
+  });
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -48,14 +48,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/events", eventsRouter);
-
-// Schedule email notifications for all events at server startup
-const Event = require("./models/event");
-Event.find().then((events) => {
-  events.forEach((event) => {
-    scheduleEmailNotification(event);
-  });
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Event = require("../models/Event");
+const Event = require("../models/event");
 
 const router = express.Router();
 
