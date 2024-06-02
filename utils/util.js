872c77ca55cd414cea982c24fa1e7a459fd3c8a7
Pradeep Shrestha <pse@logpoint.com>
2024-06-02 17:01:56 +0545

    main: debug email notification

--- utils/util.js
+++ utils/util.js
@@ -1,16 +1,28 @@
 const transporter = require("../nodemail");
 const cron = require("node-cron");
+const moment = require("moment-timezone");
 
 function scheduleEmailNotification(event) {
-  const eventDate = new Date(event.eventDate.toLocaleString());
-  const minute = eventDate.getMinutes();
-  const hour = eventDate.getHours();
-  const day = eventDate.getDate();
-  const month = eventDate.getMonth() + 1; // Month in JavaScript starts from 0
+  // const eventDate = new Date(event.eventDate.toLocaleString());
+  // const minute = eventDate.getMinutes();
+  // const hour = eventDate.getHours();
+  // const day = eventDate.getDate();
+  // const month = eventDate.getMonth() + 1; // Month in JavaScript starts from 0
   // const year = eventDate.getFullYear();
 
   // Format the cron pattern
-  const cronPattern = `0 ${minute} ${hour} ${month} ${day} *`;
+
+  // ==========
+
+  const localDate = moment.tz(event?.eventDate, moment.tz.guess());
+  // / Extract the local time components
+  const minute = localDate.minute();
+  const hour = localDate.hour();
+  const day = localDate.date();
+  const month = localDate.month() + 1; // month is 0-indexed in moment.js
+  // const dayOfWeek = localDate.day(); // 0-6 (Sun-Sat)
+  // const cronPattern = `0 ${minute} ${hour} ${day}$ {month} *`;
+  const cronPattern = `0 ${minute} ${hour} ${day} ${month} *`;
 
   console.log("date<<<<<<<<<<<<<<<<<", {
     minute,
@@ -19,7 +31,9 @@ function scheduleEmailNotification(event) {
     day,
 
     event,
-    eventDate,
+    // eventDate,
+    cronPattern,
+    localDate
   });
 
   cron.schedule(cronPattern, function () {
